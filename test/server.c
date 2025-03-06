c69e8c1f9a46cdecc35ade773e15f97941bfebb3
edaoudihicham <edaoudihicham@gmail.com>
2025-03-06 03:49:39 +0000

    newmethodtest

---
+++ test/server.c
@@ -0,0 +1,54 @@
+#include "header.h"
+
+void	handler(int signal, siginfo_t *info, void *ucontext)
+{
+	static char	c;
+	static int	bit;
+	static int	client_pid;
+
+	(void)ucontext;
+	if (client_pid != info->si_pid)
+	{
+		c = 0;
+		bit = 0;
+		client_pid = info->si_pid;
+	}
+	c = c << 1;
+	if (signal == SIGUSR1)
+		c = c | 1;
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &c, 1);
+		if (c == '\0')
+			kill(client_pid, SIGUSR2);
+		bit = 0;
+		c = 0;
+	}
+	kill(client_pid, SIGUSR1);
+}
+
+int	main(int ac, char **av)
+{
+	struct sigaction	sa;
+
+	(void)av;
+	if (ac != 1)
+		return (write(2, "Syntax: ./server\n", 17), 1);
+
+	write(1, "PID: ", 5);
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
+
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO | SA_NODEFER;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+
+	while (1)
+	{
+		pause(); // Wait for signals
+	}
+	return (0);
+}
---
+++ test/utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdaoudi <hdaoudi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 02:44:07 by hdaoudi           #+#    #+#             */
+/*   Updated: 2025/03/05 03:18:12 by hdaoudi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+		ft_putnbr(n);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else if (n >= 0 && n <= 9)
+	{
+		ft_putchar(n + '0');
+	}
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	r;
+
+	i = 0;
+	r = 0;
+	sign = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		r = (r * 10) + (str[i] - '0');
+		i++;
+	}
+	if(str[i] != '\0')
+		return (-1);
+	return (sign * r);
+}
